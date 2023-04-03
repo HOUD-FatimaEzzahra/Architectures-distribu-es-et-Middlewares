cdb7e09931a2111694153d495074cfba25ee574d
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-04-03 11:17:29 +0000

    spring security

--- Activite Pratique 4 Spring MVC JPA Hibernate Spring DATA Thymeleaf/src/main/java/ma/enset/actvitepratique4/ActvitePratique4SpringMvcJpaHibernateSpringDataThymeleafApplication.java
+++ Activite Pratique 4 Spring MVC JPA Hibernate Spring DATA Thymeleaf/src/main/java/ma/enset/actvitepratique4/ActvitePratique4SpringMvcJpaHibernateSpringDataThymeleafApplication.java
@@ -1,13 +1,42 @@
 package ma.enset.actvitepratique4;
 
+import ma.enset.actvitepratique4.entities.Patient;
+import ma.enset.actvitepratique4.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class ActvitePratique4SpringMvcJpaHibernateSpringDataThymeleafApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActvitePratique4SpringMvcJpaHibernateSpringDataThymeleafApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(
+                    new Patient(1, "ALAMI", "Hiba", new Date(), true, 1)
+            );
+            patientRepository.save(
+                    new Patient(2, "JALAL", "Ihssan", new Date(), false, 10)
+            );
+            patientRepository.findAll().forEach(p -> {
+                System.out.println(p.getNom() + " " + p.getPrenom());
+            });
+        };
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
