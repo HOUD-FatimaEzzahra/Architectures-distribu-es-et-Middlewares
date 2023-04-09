6ddca7210a9513515cba24738e2b0c641f57a398
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-04-09 02:45:00 +0000

    nsit

--- Activite Pratique 4 Spring MVC JPA Hibernate Spring DATA Thymeleaf/src/main/java/ma/enset/actvitepratique4/repositories/PatientRepository.java
+++ Activite Pratique 4 Spring MVC JPA Hibernate Spring DATA Thymeleaf/src/main/java/ma/enset/actvitepratique4/repositories/PatientRepository.java
@@ -1,10 +1,11 @@
-package ma.enset.actvitepratique4.repository;
+package ma.enset.actvitepratique4.repositories;
+
 
 import ma.enset.actvitepratique4.entities.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientRepository extends JpaRepository<Patient,Long> {
-    Page<Patient> findByNomContains(String kw, Pageable pageable);
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String mc, Pageable pageable );
 }
